5f4a4d38e09be44d12cf3fafa3d023d75f286970
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-05-28 10:24:55 +0300

    Version: Workshop-I completed

---
+++ CinemaApp.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace CinemaApp.Data
+{
+    using CinemaApp.Data.Configuration;
+    using CinemaApp.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Movie> Movies { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new MovieConfiguration());
+        }
+    }
+}
---
+++ CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Web.ViewModels.Movie;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies
+                .Select(m => new AllMoviesIndexViewModel
+                {
+                    Id = m.Id.ToString(),
+                    Title = m.Title,
+                    Genre = m.Genre,
+                    Director = m.Director,
+                    ReleaseDate = m.ReleaseDate.ToString("yyyy-MM-dd"),
+                    ImageUrl = m.ImageUrl
+                })
+                .ToListAsync();
+
+            return View(movies);
+        }
+    }
+}
